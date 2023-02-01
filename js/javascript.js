99650f0f9f4af171e62d37dcd790abea3e23c114
Ruslan0920 <rus_boy2002@ukr.net>
2023-02-01 16:45:02 +0200

    upd js

--- js/javascript.js
+++ js/javascript.js
@@ -1179,16 +1179,79 @@ console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
 
 
 // 13.3
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// console.log(keys)
+// for (const key of keys) {
+//   values.push(apartment[key]);
+//   console.log(values)
+// }
+
+
+// 14.3
+function countProps(object) {
+  // Change code below this line
+  let propCount = Object.keys(object).length;
+
+  return propCount;
+  // Change code above this line
+}
+console.log(countProps({}));
+console.log(countProps({ name: "Mango", age: 2 }));
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+
+
+// 15.3
 const apartment = {
   descr: "Spacious apartment in the city center",
   rating: 4,
   price: 2153,
 };
-const values = [];
 // Change code below this line
 const keys = Object.keys(apartment);
-console.log(keys )
-for (const key in keys) {
-  console.log(key)
-}
+console.log(keys);
+const values = Object.values(apartment);
+console.log(values);
 
+
+// 16.3
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Change code below this line
+  const values = Object.values(salaries);
+  console.log(values);
+  for (let value of values)
+    totalSalary += value;
+  // Change code above this line
+  return totalSalary;
+}
+console.log(countTotalSalary({}));
+console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+
+// 17.3
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+for (let color of colors) {
+  // console.log(color.rgb);
+  hexColors.push(color.hex);
+  console.log(hexColors);
+  rgbColors.push(color.rgb);
+  console.log(rgbColors);
+}
