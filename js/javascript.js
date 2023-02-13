b593915c0e06cb7b38c6ee76a2bdd27f9811c565
Ruslan0920 <rus_boy2002@ukr.net>
2023-02-13 16:51:45 +0200

    Update

--- js/javascript.js
+++ js/javascript.js
@@ -1321,6 +1321,79 @@ console.log(getAllPropValues("price"));
 console.log(getAllPropValues("category"));
 
 
+// 1.4
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// // Change code below this line
+
+// const result = makePizza();
+// console.log(result);
+// const pointer = makePizza;
+// console.log(pointer);
+
+
+// 2.4
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+//   console.log(pizzaName)
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Change code below this line
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+// console.log(makeMessage("Royal Grand", makePizza));
+// console.log(makeMessage("Ultracheese", deliverPizza));
+
+
+// 3.4
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}.`);
+// });
+// // Change code below this line
+
+// makePizza("Ultracheese", function eatPizza(pizzaName){
+//   console.log(`Eating pizza ${pizzaName}`)
+// });
+
+
+// 4.4
+const pizzaPalace = {
+  pizzas: ["Ultracheese", "Smoked", "Four meats"],
+  order(pizzaName, callback) {
+    
+    return callback(makePizza, onOrderError)
+  },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+pizzaPalace.order("Smoked", makePizza, onOrderError);
+pizzaPalace.order("Four meats", makePizza, onOrderError);
+pizzaPalace.order("Big Mike", makePizza, onOrderError);
+pizzaPalace.order("Vienna", makePizza, onOrderError);
+
+
 
 // const numbers = [5, 10, 15, 20, 25];
 
