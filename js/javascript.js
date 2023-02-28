6c3fa967b2b9256b9540226a03ca46cd9477528c
Ruslan0920 <rus_boy2002@ukr.net>
2023-02-28 16:34:16 +0200

    up

--- js/javascript.js
+++ js/javascript.js
@@ -1481,6 +1481,34 @@ console.log(findLongestWord("May the force be with you"));
 // ); // []
 
 // 19.3
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+// //   // Change code below this line
+//   let array = []
+  
+//   for (const product of products) {
+//     if (product[propName] === undefined) {
+//       return []
+//     }
+//     array.push(product[propName])
+//     }
+
+//   return array;
+// //   // Change code above this line
+// }
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("quantity"));
+// console.log(getAllPropValues("price"));
+// console.log(getAllPropValues("category"));
+
+
+// 20.3
 const products = [
   { name: "Radar", price: 1300, quantity: 4 },
   { name: "Scanner", price: 2700, quantity: 3 },
@@ -1488,24 +1516,26 @@ const products = [
   { name: "Grip", price: 1200, quantity: 9 },
 ];
 
-function getAllPropValues(propName) {
-//   // Change code below this line
-  let array = []
-  
+function calculateTotalPrice(productName) {
+  // Change code below this line
+  let total = 0;
   for (const product of products) {
-    if (product[propName] === undefined) {
-      return []
-    }
-    array.push(product[propName])
+    if (product[productName] === undefined) {
+      total = product.price * product.quantity;
+      console.log(total)
     }
 
-  return array;
-//   // Change code above this line
+      
+    }
+
+// return total
+  // Change code above this line
 }
-console.log(getAllPropValues("name"));
-console.log(getAllPropValues("quantity"));
-console.log(getAllPropValues("price"));
-console.log(getAllPropValues("category"));
+console.log(calculateTotalPrice("Blaster"));
+console.log(calculateTotalPrice("Radar"));
+console.log(calculateTotalPrice("Droid"));
+console.log(calculateTotalPrice("Grip"));
+console.log(calculateTotalPrice("Scanner"));
 
 
 // 1.4
