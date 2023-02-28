a8f69d0312c76935f2c1b63f15bd5b5e01d71d7f
Ruslan0920 <rus_boy2002@ukr.net>
2023-02-28 16:53:36 +0200

    upd

--- js/javascript.js
+++ js/javascript.js
@@ -1509,27 +1509,42 @@ console.log(findLongestWord("May the force be with you"));
 
 
 // 20.3
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
-];
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
 
-function calculateTotalPrice(productName) {
-  // Change code below this line
-  let total = 0;
-  for (const product of products) {
-    if (product[productName] === undefined) {
-      total = product.price * product.quantity;
-      console.log(total)
-    }
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
+//   let total = 0;
+//   for (const product of products) {
+//     if (product[productName] === undefined) {
+//       total = product.price * product.quantity;
+//       console.log(total)
+//     }
 
       
-    }
+//     }
 
 // return total
   // Change code above this line
+
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  for (const product of products)
+    if (product.name === productName)
+      return product.price * product.quantity;
+  return 0;
+
 }
 console.log(calculateTotalPrice("Blaster"));
 console.log(calculateTotalPrice("Radar"));
