3831465c10f984201260dbb53a57b9b917aac535
Ruslan0920 <rus_boy2002@ukr.net>
2023-03-15 16:59:41 +0200

    upd

--- js/javascript.js
+++ js/javascript.js
@@ -4175,7 +4175,7 @@ const pizzaPalace = {
     return this.pizzas.includes(pizzaName);
   },
   order(pizzaName) {
-    const isPizzaAvailable = checkPizza(pizzaName);
+    const isPizzaAvailable = this.checkPizza(pizzaName);
 
     if (!isPizzaAvailable) {
       return `Sorry, there is no pizza named «${pizzaName}»`;
@@ -4185,11 +4185,130 @@ const pizzaPalace = {
   },
   // Change code above this line
 };
-console.log(pizzaPalace.checkPizza())
+// console.log(pizzaPalace);
+// console.log(pizzaPalace.checkPizza());
+// console.log(pizzaPalace.order)
 console.log(pizzaPalace.order("Smoked"));
 console.log(pizzaPalace.order("Four meats"));
 console.log(pizzaPalace.order("Big Mike"));
 console.log(pizzaPalace.order("Viennese"));
+
+
+// 2.5
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+
+// 3.5
+const historyService = {
+  orders: [
+    { email: "jacob@hotmail.com", dish: "Burrito" },
+    { email: "solomon@topmail.net", dish: "Burger" },
+    { email: "artemis@coldmail.net", dish: "Pizza" },
+    { email: "solomon@topmail.net", dish: "Apple pie" },
+    { email: "jacob@hotmail.com", dish: "Taco" },
+  ],
+  // Change code below this line
+  getOrdersLog() {
+    return this.orders
+      .map(order => `email: ${order.email} dish: ${order.dish}`)
+      .join(" - ");
+  },
+  getEmails() {
+    const emails = this.orders.map(order => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
+  },
+  getOrdersByEmail(email) {
+    return this.orders.filter(order => order.email === email);
+  },
+  // Change code above this line
+};
+console.log(historyService.getOrdersLog());
+console.log(historyService.getEmails());
+console.log(historyService.getOrdersByEmail("solomon@topmail.net"));
+console.log(historyService.getOrdersByEmail("artemis@coldmail.net"));
+
+
+// 4.5
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Change code below this line
+
+// const child = Object.create(parent);
+
+// // Change code above this line
+// child.name = "Jason";
+// child.age = 27;
+
+// console.log(parent.hasOwnProperty("heritage"));
+// console.log(child.hasOwnProperty("name"));
+// console.log(child.hasOwnProperty("age"));
+// console.log(child.hasOwnProperty("surname"));
+// console.log(child.hasOwnProperty("heritage"));
+// console.log(parent.isPrototypeOf(child));
+
+
+// 5.5
+const ancestor = {
+  name: "Paul",
+  age: 83,
+  surname: "Dawson",
+  heritage: "Irish",
+};
+// Change code below this line
+
+const parent = Object.create(ancestor);
+parent.name = "Stacey";
+parent.surname = "Moore";
+parent.age = 54;
+
+const child = Object.create(parent);
+child.name = "Jason";
+child.age = 27;
+
+// Change code above this line
+console.log(ancestor.isPrototypeOf("parent"));
+console.log(parent.isPrototypeOf("child"));
+console.log(ancestor.hasOwnProperty("surname"));
+console.log(parent.hasOwnProperty("surname"));
+console.log(child.hasOwnProperty("surname"));
+console.log(ancestor.hasOwnProperty("heritage"));
+console.log(parent.hasOwnProperty("heritage"));
+console.log(child.hasOwnProperty("heritage"));
+
 
 
 
