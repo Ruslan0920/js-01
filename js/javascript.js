0108858ba160f3d796fd25cbb835304baa99fa1e
Руслан <rus_boy2002@ukr.net>
2023-03-20 23:28:16 +0200

    upd

--- js/javascript.js
+++ js/javascript.js
@@ -4404,7 +4404,24 @@ console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 
 // 11.5
+class StringBuilder {
+  constructor(value) {
+    this.value = value;
+  }
 
+  getValue() {
+    return this.value;
+  }
+  padEnd(str) {
+    this.value += str;
+  }
+  padStart(str) {
+    this.value = str + this.value;
+  }
+  padBoth(str) {
+    this.value = str + this.value + str;
+  }
+}
 
 
 // Change code above this line
