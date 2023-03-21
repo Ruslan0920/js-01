facb055d86a98daf55977ef2e275b6fdc48ce666
Руслан <rus_boy2002@ukr.net>
2023-03-22 00:04:54 +0200

    19

--- js/javascript.js
+++ js/javascript.js
@@ -4566,35 +4566,123 @@ console.log(builder.getValue()); // "=^.^="
 
 
 // 16.5
+// class Car {
+//   // Change code below this line
+//   static MAX_PRICE = 50000;
+
+//   #price;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (newPrice <= Car.MAX_PRICE)
+//       this.#price = newPrice;    
+//   }
+//   // Change code above this line
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+
+// 17.5
 class Car {
+  static #MAX_PRICE = 50000;
   // Change code below this line
-  static MAX_PRICE = 50000;
-
-  #price;
-
+  static checkPrice(price) {
+    if (price > Car.#MAX_PRICE) {
+    return "Error! Price exceeds the maximum"
+    }
+    return "Success! Price is within acceptable limits"
+}
+  // Change code above this line
   constructor({ price }) {
-    this.#price = price;
+    this.price = price;
+  }
+}
+
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
+
+console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+
+// 18.5
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// // Change code below this line
+// class Admin extends User {
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser"
+//   }
+// }
+// console.log(Admin.AccessLevel.BASIC);
+// console.log(Admin.AccessLevel.SUPERUSER)
+
+
+// 19.5
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
   }
 
-  get price() {
-    return this.#price;
+  get email() {
+    return this.email;
   }
 
-  set price(newPrice) {
-    if(this.#price > MAX_PRICE)
-    this.#price = newPrice;
+  set email(newEmail) {
+    this.email = newEmail;
   }
+}
+
+class Admin extends User {
+  // Change code below this line
+  accessLevel;
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+}
   // Change code above this line
 }
 
-const audi = new Car({ price: 35000 });
-console.log(audi.price); // 35000
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
 
-audi.price = 49000;
-console.log(audi.price); // 49000
-
-audi.price = 51000;
-console.log(audi.price); // 49000
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
 
 
 
